0b12ebe77da76970a13ee25a30621149bc69cea7
Sentientplatypus <trexxxxxxxxxxy@gmail.com>
2023-08-16 22:15:23 -0400

    finished update fn

--- app/services/static/js/update.js
+++ app/services/static/js/update.js
@@ -2,7 +2,7 @@ async function updatevals() {
     // Generate placeholder values for demonstration purposes
     dictionary = (await httpGet("https://financialmodelingprep.com/api/v3/quote-short/" + document.URL.split("/").pop() + "?apikey=b0446da02c01a0943a01730dc2343e34"));
     var newChange = dictionary[0].price;
-    var newMarketStatus = Math.random() > 0.5 ? "Open" : "Closed";
+    var newMarketStatus = (await httpGet("/getChangestr/" + document.URL.split("/").pop()));
 
     // Update the change and market status elements
     $(".change").text(newChange);
