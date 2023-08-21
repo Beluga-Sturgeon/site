c400727cd24156702c8677182c89866113b5c847
Sentientplatypus <trexxxxxxxxxxy@gmail.com>
2023-08-20 22:00:52 -0400

    added thing

--- app/services/static/js/updatevalues.js
+++ app/services/static/js/updatevalues.js
@@ -1,24 +1,27 @@
 async function updatevals() {
-    // Generate placeholder values for demonstration purposes
-    dictionary = (await httpGet("https://financialmodelingprep.com/api/v3/quote-short/" + document.URL.split("/").pop() + "?apikey=b0446da02c01a0943a01730dc2343e34"));
-    var newChange = dictionary[0].price;
-    var newMarketStatus = (await httpGet("/getChangestr/" + document.URL.split("/").pop()));
-
-    // Update the change and market status elements
-    $(".change").text(newChange);
-    $(".market-status").text(newMarketStatus);
+    try {
+      // Generate placeholder values for demonstration purposes
+      const quoteData = await httpGet("https://financialmodelingprep.com/api/v3/quote-short/" + document.URL.split("/").pop() + "?apikey=b0446da02c01a0943a01730dc2343e34");
+      const newChange = quoteData[0].price;
+      const newMarketStatus = await httpGet("/getChangestr/" + document.URL.split("/").pop());
+  
+      // Update the change and market status elements
+      $(".change").text(newChange);
+      $(".market-status").text(newMarketStatus);
+    } catch (error) {
+      console.error('Error updating values:', error);
+    }
+  }
+  
+async function updateLoop() {
+    await updatevals();
+    setTimeout(updateLoop, 60000); // 60000 milliseconds = 1 minute
 }
 
-// Call the function initially
-await updatevals();
+console.log("UPDATEEEE");
 
-// Set up the interval to call the function every minute
-setInterval(await updatevals, 60000); // 60000 milliseconds = 1 
-
-console.log("UPDATEEEE")
-
-
-window.onload = function() {
+window.onload = function () {
     console.log("I AM READING THE SCRIPT");
-   document.getElementById('bodytext').innerHTML = "I AM READING THE SCRIPT";
+    document.getElementById('bodytext').innerHTML = "I AM READING THE SCRIPT";
+    updateLoop(); // Start the update loop after the page loads
 };
